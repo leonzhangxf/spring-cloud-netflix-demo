cc42e9b1d41fddbbdb7b17fe190b6820e251bea8
leon_zhangxf <leonzhangxf@gmail.com>
2018-11-28 20:03:24 +0800

    gateway 网关服务

---
+++ gateway-server-dist/src/main/java/com/leonzhangxf/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.leonzhangxf;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(GatewayApplication.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
