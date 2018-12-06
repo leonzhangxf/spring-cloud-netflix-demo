c36ad7748f8a1d1f75b535fb94bb4db06b0a8cae
leon_zhangxf <leonzhangxf@gmail.com>
2018-12-06 20:51:15 +0800

    auth server 正常配置流程

--- auth-server-dist/src/main/java/com/leonzhangxf/api/OauthApi.java
+++ auth-server-dist/src/main/java/com/leonzhangxf/api/OauthApi.java
@@ -1,9 +1,7 @@
 package com.leonzhangxf.api;
 
-import com.leonzhangxf.dao.UserRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +11,9 @@ import java.security.Principal;
 @RestController
 public class OauthApi {
 
-    private UserRepository userRepository;
-
     @ApiOperation("userinfo")
     @RequestMapping("/userinfo")
     public Principal userInfo(Principal user) {
-//        return userRepository.findByUsername(user.getName());
         return user;
-    }
-
-    @Autowired
-    public void setUserRepository(UserRepository userRepository) {
-        this.userRepository = userRepository;
     }
 }
